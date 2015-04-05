662fa0ec4ef7da400bf460e6d15273a8f6327390
Kunal Bhutani <kunalbhutani@gmail.com>
2015-04-05 16:22:46 -0700

    Updated with correlation stats and scripts to create grpahs for metabolites with correlation < 0.5

--- R/metabolome_qc.R
+++ R/metabolome_qc.R
@@ -1,5 +1,5 @@
 #Author: Kunal Bhutani
-#Date: March 31s, 2015
+#Date: March 31st, 2015
 #This script does basic QC for the metabolome data. 
 
 # Libraries
@@ -9,7 +9,9 @@ library(xlsx)
 local.path = "/home/kunal/tscc_projects/tanner/data/family3/metabolome"
 date = "04042015"
 analysis.path = file.path(local.path, "analysis", date)
+tn.path = file.path(analysis.path, "technical_noise")
 path.to.Rdata = file.path(local.path, "metabolome.data_032715.RData")
+rownames(metab.info) = metab.info[,'CHEMICAL.ID']
 
 
 # Loading in Data
@@ -42,7 +44,7 @@ FindOverlappingMetabolites <- function(runs = 1:length(plasma)) {
 }
 
 CreateSampleDfs <- function(sample.runs.df, metabolites) {
-  # Creates a sample specific dataframe based on the values in sample.runs.df
+  # Creates a list of sample specific dataframes based on the values in sample.runs.df
   
   ProcessSample <- function(sample) {
     sample.runs = sample.runs.df[sample, ]
@@ -59,28 +61,14 @@ CreateSampleDfs <- function(sample.runs.df, metabolites) {
   sample.dfs
 }
 
-save.xlsx <- function (file, ...)
-{
-  # Copied from http://www.r-bloggers.com/quickly-export-multiple-r-objects-to-an-excel-workbook/
-  require(xlsx, quietly = TRUE)
-  objects <- list(...)
-  fargs <- as.list(match.call(expand.dots = TRUE))
-  objnames <- as.character(fargs)[-c(1, 2)]
-  nobjects <- length(objects)
-  for (i in 1:nobjects) {
-    if (i == 1)
-      write.xlsx(objects[[i]], file, sheetName = objnames[i])
-    else write.xlsx(objects[[i]], file, sheetName = objnames[i],
-                    append = TRUE)
-  }
-  print(paste("Workbook", file, "has", nobjects, "worksheets."))
+CreateMetaboliteDfs <- function(sample.runs.df, metabolites) {
+  # TODO: Create Metabolite Dfs, makes it easier for later analysis for specific metabolites
 }
 
 # Analysis
 
 
 # TODO:
-# Create correlation based on sample DFs
 # Find how many metabolites pass based on correlation test vs coefficient of variation
 # Use different samples as the scale
 # Create beautiful plots
@@ -101,41 +89,58 @@ individuals = sapply(rownames(sample.runs.df), function(x) substr(x, 6, 8))
 run.info = !is.na(sample.runs.df[names(individuals[order(individuals)]),])
 general.fn = file.path(analysis.path, "general.xlsx")
 individual.info = table(individuals)
-save.xlsx(general.fn, run.info, individual.info)
+write.xlsx(individual.info, general.fn, sheetName = "Individual Info")
+write.xlsx(run.info, general.fn, sheetName = "Run Info",
+           append = TRUE)
+
 
 # Find the correlation for each metabolite between sample 1 and 2
-correlations = diag(cor(t(as.matrix(sample.dfs[[1]])), t(as.matrix(sample.dfs[[2]]))))
-hist(correlations)
+FindCorrelation <- function(sample1.df, sample2.df) {
+  overlapping.runs = intersect(colnames(sample1.df),
+                               colnames(sample2.df))
+  correlation = diag(cor(t(as.matrix(sample1.df[,overlapping.runs])),
+                         t(as.matrix(sample2.df[,overlapping.runs]))))
+  correlation
+}
 
-# Subselect high confidence metabolites with high correlation for
-# continued analysis
-hf_metabolites = overlapping.metabolites[which(correlations > 0.9)]
+sample1 = 'X001_002_Metabolites_Plasma_JCVI.00001_04.16.14'
+sample2 = 'X001_002_Metabolites_Plasma_JCVI.00002_04.16.14'
+correlations = FindCorrelation(sample.dfs[[sample1]], sample.dfs[[sample2]])
+names(correlations) = metab.info[names(correlations),]$BIOCHEMICAL
+
+corr.fn = file.path(tn.path, sprintf("corr_%s_v_%s", sample1, sample2))
+png(corr.fn)
+title = sprintf("JCVI.00001 vs JCVI.00002 \n Correlation for Metabolites \n (%d)", 
+                length(correlations))
+hist(correlations, main=title, cex=0.5)
+dev.off()
+
+# Create metabolite specific scatter plots for ones that have low correlation
+# TODO: Clean up code, use metabolite specific dfs.
+sample1.df = sample.dfs[[sample1]]
+sample2.df = sample.dfs[[sample2]]
+overlapping.runs = intersect(colnames(sample1.df),
+                             colnames(sample2.df))
+ 
+sample1.values = sample1.df[overlapping.metabolites[which(correlations < 0.5)],
+                                       overlapping.runs]
+sample2.values = sample2.df[overlapping.metabolites[which(correlations < 0.5)],
+                                       overlapping.runs]
+rownames(sample1.values) = metab.info[rownames(sample1.values),]$BIOCHEMICAL
+rownames(sample2.values) = rownames(sample1.values)
+for (i in 1:length(sample1.values)){
+  metabolite = rownames(sample1.values)[i]
+  fn = file.path(tn.path, sprintf("%s_%s_v_%s", metabolite, sample1, sample2))
+  png(fn)
+  title = sprintf("%s for JCVI.00001 vs JCVI.00002 \n Corr: %0.2f", metabolite, correlations[metabolite])
+  plot(as.numeric(sample1.values[metabolite,]), as.numeric(sample2.values[metabolite,]),
+       xlab="Raw Counts JCVI.00001", ylab="Raw Counts JCVI.00002", main=title )
+  dev.off()
+}
+
+# Calculate Coefficient of Variation based on Scaled for TP2
 
 # Create new scaled values based on sample 1 only.
-all_samples = colnames(plasma[[6]])
-scaled.samples = NULL
-for(i in 1:length(all_samples)) {
-  sample_df = NULL
-  for (j in 1:length(plasma)){
-    if (all_samples[i] %in% colnames(plasma[[j]])) {
-      run_df = plasma[[j]][hf_metabolites, all_samples[i]]
-      sample1_df = plasma[[j]][hf_metabolites, sample1]
-      run_df = run_df/sample1_df
-      sample_df= cbind(sample_df, run_df)
-    }
 
-  }
-  rownames(sample_df) = hf_metabolites
-  scaled.samples[length(scaled.samples)+1] = list(sample_df)
-}
-names(scaled.samples) = all_samples
-
-cv.samples = NULL
-for (i in 1:length(scaled.samples)) {
-  sample_sd = apply(as.matrix(scaled.samples[[i]]), 1, sd)
-  sample_mean = rowMeans(scaled.samples[[i]])
-  cv_df = sample_sd/sample_mean
-  cv.samples[length(cv.samples)+1] = list(cv_df)
-}
-
+# Coefficient of Variation: sd/mean
 
