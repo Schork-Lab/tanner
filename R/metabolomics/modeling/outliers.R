6ec1be97f3fbec18f83561d12d69152007ac9d1b
Kunal Bhutani <kunalbhutani@gmail.com>
2015-05-06 13:00:10 -0700

    First commit of outliers.R which is attempting to find outliers across the samples for the main patient.
    Multiple ways to do this, but the first approach is to standardize the data,
    and then find outliers based on z-scores.
    
    Note that this only considers Run 6.
    
    Issue #3

---
+++ R/metabolomics/modeling/outliers.R
@@ -0,0 +1,25 @@
+#Author: Kunal Bhutani
+#Date: May 6, 2015
+#Purpose: Detect metabolites that are deviating from baseline for an individual
+
+# Load data
+source("R/metabolomics/load.R")
+
+# Set paths
+local.path = config$paths$local
+code.path = file.path(local.path, 'code/R/')
+
+# Extract only Run 6
+samples = GetAllSamples()
+sample.runs.df = CreateSampleRunsDf(samples)
+run.6.sample.runs.df = sample.runs.df[,"6",drop=FALSE]
+run.6.sample.runs.df = run.6.sample.runs.df[which(!is.na(run.6.sample.runs.df)), , drop=F]
+run.6.metabolites = FindOverlappingMetabolites(c(6))
+run.6.sample.dfs = CreateSampleDfs(run.6.sample.runs.df, run.6.metabolites)
+run.6.metabolite.df = CreateMetaboliteDfs(run.6.sample.runs.df, run.6.sample.dfs,run.6.metabolites)
+rownames(run.6.metabolite.df) = run.6.metabolite.df$Xsample.cols
+run.6.metabolite.df = run.6.metabolite.df[,3:dim(run.6.metabolite.df)[2]]
+
+# Scale across samples by subtracting out mean
+scaled.metabolite.df = t(apply(run.6.metabolite.df, 1, function(x) { scale(x, scale=FALSE)}))
+colnames(scaled.metabolite.df) = colnames(run.6.metabolite.df)
