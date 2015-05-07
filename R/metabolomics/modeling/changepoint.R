dc2a00593999dd2c67536c5341bab3cb6692766d
Kunal Bhutani <kunalbhutani@gmail.com>
2015-05-07 16:53:35 -0700

    Added code to look at change points for different scaling factors: 1. Median scaled 2. Z-Scores 3. Log Transformed
    The median scaled values are the ones that is used by Metabolomics and
    is the most stable with this data as well. The output graphs can be seen
    in analysis/metabolomics/05072015 .
    
    Issue #4

--- R/metabolomics/modeling/changepoint.R
+++ R/metabolomics/modeling/changepoint.R
@@ -7,6 +7,15 @@ library(changepoint)
 # Load data
 source("R/metabolomics/load.R")
 
+# Output Directory
+local.path = config$paths$local
+today <- Sys.Date()
+date = format(today, format="%m%d%Y")
+analysis.path = file.path(local.path, "analysis", "metabolomics", date)
+dir.create(analysis.path)
+cp.path = file.path(analysis.path, "change_points")
+dir.create(cp.path)
+
 CalculateChangePoints = function(scaled.metabolite.df) {
   # Change point analysis
   # http://www.jstatsoft.org/v58/i03/paper
@@ -37,7 +46,10 @@ CalculateChangePoints = function(scaled.metabolite.df) {
                                meanvar.BinSeg = I(sapply(mv.binseg, cpts)))
   
   changepoints = cbind(m.changepoints, v.changepoints, mv.changepoints)
+
   changepoints = list("changepoints"= changepoints,
+                      "samples" = rownames(scaled.metabolite.df),
+                      "metabolites" = colnames(not.na.metabolites),
                       "m.pelt"= m.pelt, "m.binseg"= m.binseg,
                       "v.pelt"= v.pelt, "v.binseg"= v.binseg,
                       "mv.pelt"= mv.pelt, "mv.binseg"= mv.binseg)
@@ -45,15 +57,61 @@ CalculateChangePoints = function(scaled.metabolite.df) {
   
 }
 
+PlotChangePoints = function(changepoints, output.prefix, output.suffix, title.prefix,
+                            ylabel="Raw Counts") {
+  
+  PlotMetabolite = function (metabolite) {
+    metabolite.id = substr(metabolite, 2, nchar(metabolite))
+    biochemical = metab.info[metabolite.id, 'BIOCHEMICAL']
+    out.fn = paste(output.prefix, biochemical, output.suffix, sep="")
+    jpeg(out.fn)
+    labels = sapply(rownames(scaled.metabolite.df),
+                    function(x) {tail(strsplit(x, "_")[[1]], n=1)})
+    plot(changepoints$m.pelt[[metabolite]], type="b", cpt.col="red", xlab="", ylab="", xaxt="n", yaxt="n")
+    par(new=T)
+    plot(changepoints$v.pelt[[metabolite]], type="b", cpt.col="blue", xlab="", ylab="", xaxt="n", yaxt="n")
+    par(new=T)
+    plot(changepoints$v.pelt[[metabolite]], type="b", cpt.col="green", xaxt="n",
+         xlab="Date", ylab=ylabel, main=paste(title.prefix, biochemical))
+    axis(1, at=1:length(labels), labels=labels, cex.axis=0.95)
+    legend("topright", lty=c(1,1,1), lwd=c(2.5,2.5,2.5),col=c("red", "blue", "green"),
+           legend=c("Mean Change","Var Changes", "MeanVar Changes")) 
+    dev.off()
+    
+  }
+  
+  sapply(changepoints$metabolites, PlotMetabolite)
+  
+}
+
+
 # Initial analysis only on run 6
 run.6 = LoadData(c(6))
-scaled.metabolite.df = run.6$metabolite.df[,3:dim(run.6$metabolite.df)[2]]
-rownames(scaled.metabolite.df) = run.6$metabolite.df$Xsample.cols
+metabolite.df = run.6$metabolite.df[,3:dim(run.6$metabolite.df)[2]]
+rownames(metabolite.df) = run.6$metabolite.df$Xsample.cols
 
-# Scale across samples by log-transforming data and then subtracting out mean
-#scaled.metabolite.df = t(apply(scaled.metabolite.df, 1, function(x) { scale(log(x), scale=F)}))
+output.prefix = file.path(cp.path, "run6-")
+title.prefix = "Run 6: "
+# Scale across samples by log-transforming data
+scaled.log.metabolite.df = log(metabolite.df)
+changepoints = CalculateChangePoints(scaled.log.metabolite.df)
+output.suffix = "-log.jpg"
+ylabel = "Log Raw Score"
+PlotChangePoints(changepoints, output.prefix, output.suffix, title.prefix, ylabel)
 
-# Since these samples are run on the same run, no need to scale the data. A simple log transformation
-# is sufficient.
-scaled.metabolite.df = log(scaled.metabolite.df)
-changepoints = CalculateChangePoints(scaled.metabolite.df)
+# Using standard scaling with mean = 0, sd = 1 here.
+scaled.z.score.metabolite.df = scale(metabolite.df)
+changepoints = CalculateChangePoints(scaled.z.score.metabolite.df)
+output.suffix = "-zscore.jpg"
+ylabel = "Z-Score"
+PlotChangePoints(changepoints, output.prefix, output.suffix, title.prefix, ylabel)
+
+# Scale by median
+scaled.median.metabolite.df = apply(scaled.metabolite.df, 2, function(x) { x/median(x)})
+changepoints = CalculateChangePoints(scaled.median.metabolite.df)
+output.suffix = "-median.jpg"
+ylabel = "Median Scaled"
+PlotChangePoints(changepoints, output.prefix, output.suffix, title.prefix, ylabel)
+
+
+
